fb7cbaee7532231ed5534fcd615f3ae0251ea69e
koihsw <156396653+koihsw@users.noreply.github.com>
2026-02-22 18:16:23 +1100

    Added more code
    Retrieves the entire list of chapters per page. One page has 50 chapters, so it will output 50 chapters per page in order.

--- scraper.py
+++ scraper.py
@@ -3,30 +3,52 @@ from bs4 import BeautifulSoup
 
 
 domain = "https://novelfull.net"
+
+# target url
 MAINURL = "https://novelfull.net/a-regressors-tale-of-cultivation.html"
 url = MAINURL
-urlResponse = requests.get(url).text
 
+# chapter count 
+chCount = 0
+
+# page number
+pageNum = 0
+pageUrl = f"?page={pageNum}"
+
+urlResponse = requests.get(url).text
 soup = BeautifulSoup(urlResponse, 'lxml')
 
-lChapters = soup.find("ul", class_="l-chapters")
+# latest chapters list, first item in the list is the latest chapter
+lChaptersList = soup.find("ul", class_="l-chapters")
+lastestCh = lChaptersList.find("a")
 
-print(lChapters)
+latestFound = False
 
-chapterList = soup.find_all("ul", class_="list-chapter")
+print(lastestCh['title'])
 
 links = []
+while latestFound == False:
 
-for element in range(len(chapterList)):
+    pageNum += 1
+    pageUrl = f"?page={pageNum}"
+    url = MAINURL + pageUrl
+
+    urlResponse = requests.get(url).text
+    soup = BeautifulSoup(urlResponse, 'lxml')
+
+    chapterList = soup.find_all("ul", class_="list-chapter")
     
-    temp = chapterList[element].find_all("a")
-    for chapterNum in range(len(temp)):
-        print(temp[chapterNum]['title'])
+    for element in range(len(chapterList)):
+        
+        temp = chapterList[element].find_all("a")
+        for chapterNum in range(len(temp)):
+            print(temp[chapterNum]['title'])
+            chCount += 1
+            if temp[chapterNum]['title'] == lastestCh['title']:
+                print("found")
+                latestFound = True
+            # links.append(temp[chapterNum])
 
-    links.append(chapterList[element].find_all("a"))
-    # print(chapterList[element].find_all("a")[0]['title'])
-
-    # print(links[0]['title'])
-
-# there are two arrays inside "links"
-print(links[0][0]['title'])
+    print(chCount)
+    print(pageNum)
+    print(url)
